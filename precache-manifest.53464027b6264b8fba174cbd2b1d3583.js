5faba9da4ecbd5be739185042ed3b5f642cb490f
mmshabab90 <mmshabab90@gmail.com>
2019-06-20 19:05:26 +0200

    Updates

---
+++ precache-manifest.53464027b6264b8fba174cbd2b1d3583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3549ad46cb33fd917dfd121d2dee33e1",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "a5197b5a637c3e15bf62",
+    "url": "/my-profile/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "17a8a12a197d0fee7f68",
+    "url": "/my-profile/static/css/main.5be8d1df.chunk.css"
+  },
+  {
+    "revision": "a5197b5a637c3e15bf62",
+    "url": "/my-profile/static/js/2.f03e7fa7.chunk.js"
+  },
+  {
+    "revision": "17a8a12a197d0fee7f68",
+    "url": "/my-profile/static/js/main.81716b8f.chunk.js"
+  },
+  {
+    "revision": "635b166ff5ddd166f46b",
+    "url": "/my-profile/static/js/runtime~main.af69c787.js"
+  }
+]);
