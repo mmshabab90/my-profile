68b71456818d101a9c97f645f7d59db3fe906267
mmshabab90 <mmshabab90@gmail.com>
2019-06-21 20:15:17 +0200

    Updates

--- precache-manifest.048ace9984befe177820d31a46df96dd.js
+++ precache-manifest.048ace9984befe177820d31a46df96dd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cda50e3e22de2709cb4379db49562f57",
+    "revision": "d32f7c2c60d866e082c3b7faa7e7bc82",
     "url": "/my-profile/index.html"
   },
   {
-    "revision": "cfdbe204191d28046c5e",
+    "revision": "b211d4a1175cff2665f6",
     "url": "/my-profile/static/css/2.60a6b8e0.chunk.css"
   },
   {
-    "revision": "9c47ae34be591840f737",
+    "revision": "0978e8f72ab8fc9fa099",
     "url": "/my-profile/static/css/main.5be8d1df.chunk.css"
   },
   {
-    "revision": "cfdbe204191d28046c5e",
-    "url": "/my-profile/static/js/2.5d490c9a.chunk.js"
+    "revision": "b211d4a1175cff2665f6",
+    "url": "/my-profile/static/js/2.ce217408.chunk.js"
   },
   {
-    "revision": "9c47ae34be591840f737",
-    "url": "/my-profile/static/js/main.6c5c355d.chunk.js"
+    "revision": "0978e8f72ab8fc9fa099",
+    "url": "/my-profile/static/js/main.1b8d4362.chunk.js"
   },
   {
     "revision": "64d5eb51a03ab259e6d3",
